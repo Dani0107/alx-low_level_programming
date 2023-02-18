97c8823d4fc9dba8002b6300f61821f230367f95
Dani0107 <oluwaseundaniel30@gmail.com>
2023-02-18 04:30:40 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - print the code using puts
@@ -9,5 +9,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
